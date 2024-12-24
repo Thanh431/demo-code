980c69353fbcd771f01546479c6b76893314d650
Thanh431 <trintse180561@fpt.edu.vn>
2024-12-24 18:48:24 +0700

    upcode

---
+++ tang-crush-main/config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Dù khoảng cách không thể chia cắt chúng ta
+Nhưng trong trái tim anh, em luôn là người đặc biệt nhất
+Chúc em một kỳ Giáng sinh ấm áp, đầy yêu thương
+Và hy vọng một ngày gần nhất ta sẽ bên nhau
+
+Hy vọng em thích cách viết này!`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: ' không phải:3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì anh quá đẹp chai  <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chúng ta bắt đầu trò chơi nàooo😙 ',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://quizizz.com/join?gc=307775&source=liveDashboard' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
